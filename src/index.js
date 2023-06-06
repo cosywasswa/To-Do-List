096ce23547de07da2097489f52baa19279127585
Cosmas Wasswa <cosywas@gmail.com>
2023-06-06 11:25:18 +0300

    Updated javascript by removing errors

--- src/index.js
+++ src/index.js
@@ -1,65 +1,61 @@
-import _ from 'lodash';
 import './style.css';
 
 const toDoTask = [
-    {
-        discription: "Clean the house",
-        completed: true,
-        index: 5
+  {
+    discription: 'Clean the house',
+    completed: true,
+    index: 5,
 
-    },
-    {
-        discription: "Complete the project",
-        completed: false,
-        index: 6
-    },
-    {
-        discription: "pick the kids",
-        completed: true,
-        index: 3
+  },
+  {
+    discription: 'Complete the project',
+    completed: false,
+    index: 6,
+  },
+  {
+    discription: 'pick the kids',
+    completed: true,
+    index: 3,
 
-    },
-    {
-        discription: "Call michael",
-        completed: false,
-        index: 4
-    },
-    {
-        discription: "Make project changes",
-        completed: false,
-        index: 1
-    },
-    {
-        discription: "Join zoom meeting",
-        completed: true,
-        index: 2
-    }
-]
+  },
+  {
+    discription: 'Call michael',
+    completed: false,
+    index: 4,
+  },
+  {
+    discription: 'Make project changes',
+    completed: false,
+    index: 1,
+  },
+  {
+    discription: 'Join zoom meeting',
+    completed: true,
+    index: 2,
+  },
+];
 
-let sorted = toDoTask.sort((a, b) => a.index - b.index);
-
+const sorted = toDoTask.sort((a, b) => a.index - b.index);
 const toDoList = () => {
-    for (let i = 0; i < sorted.length; i++) {
-        const container = document.getElementById('list-items');
-        let checkbox = document.createElement('input');
-        const element = document.createElement('li');
-       
-        checkbox.type = 'checkbox';
-        checkbox.classList.add('check');
-        container.appendChild(checkbox);
+  for (let i = 0; i < sorted.length; i++) {
+    const container = document.getElementById('list-items');
+    const checkbox = document.createElement('input');
+    const element = document.createElement('li');
 
-        if (sorted[i].completed) {
-            element.classList.add('completed');
-            checkbox.checked = true;
-        } else {
-            element.classList.add('not-completed');
-        }
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('check');
+    container.appendChild(checkbox);
 
-
-        element.classList.add('list');
-        element.innerHTML = `<p>${sorted[i].discription}</p><hr>`;
-        container.appendChild(element);
+    if (sorted[i].completed) {
+      element.classList.add('completed');
+      checkbox.checked = true;
+    } else {
+      element.classList.add('not-completed');
     }
 
-}
-window.addEventListener("load", toDoList);
+    element.classList.add('list');
+    element.innerHTML = `<p>${sorted[i].discription}</p><hr>`;
+    container.appendChild(element);
+  }
+};
+window.addEventListener('load', toDoList);
